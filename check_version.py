6cb977cf1a44b41ad734ec22ea5aa4a8bb48c9d7
Isangedal <isaknikolai10@gmail.com>
2022-05-21 00:43:49 +0200

    1.3

--- check_version.py
+++ check_version.py
@@ -1,4 +1,54 @@
-def run(version, appname, link):
+import requests
+
+def run(version, appname):
+    # Get the html from "http://www.isangedal.7m.pl/websites/version/{appname}.php"
+    # This will return a new url. Go to that url and get the html.
+    # That html will contain the version number.
+    # If the version number is equal to "version", return "OK"
+    # If the version number is LESS than "version", return "AHEAD"
+    # If the version number is GREATER than "version", print "link" which is the link to download the latest version then return "OUTDATED"
+    # If any other error occurs, return "ERROR"
+    # Let's do it!
+    r = requests.get(f"http://www.isangedal.7m.pl/websites/version/{appname}.php")
+
+    if r.status_code == 200:
+        r2 = requests.get(r.text)
+
+        text = float(r2.text)
+        #print(text, version)
+
+        if r2.status_code == 200:
+            if version == text:
+                return "OK"
+            elif version > text:
+                return "AHEAD"
+            else:
+                return "OUTDATED"
+        else:
+            return "ERROR"
+    else:
+        return "ERROR"
+    
+    try:
+        r = requests.get(f"http://www.isangedal.7m.pl/websites/version/{appname}.php")
+
+        if r.status_code == 200:
+            r2 = requests.get(r.text)
+
+            if r2.status_code == 200:
+                if r2.text <= version:
+                    return "OK"
+                else:
+                    print(link)
+                    return "OUTDATED"
+            else:
+                return "ERROR"
+        else:
+            return "ERROR"
+    except:
+        return "ERROR"
+
+'''def run(version, appname, link):
     import requests
 
     try:
@@ -24,4 +74,4 @@ def run(version, appname, link):
         else:
             print(f"You are on the newest version. Enjoy!")
     else:
-        print("Was unable to check for new version.")
+        print("Was unable to check for new version.")'''
--- exe/README.txt
+++ exe/README.txt
@@ -2,6 +2,5 @@ This is if you want to make your own exe file if you don't trust the original on
 
 Step 1: Make sure you have python installed. If not, go to: https://www.python.org/downloads
 Step 2: Run "build.bat"
-Step 3: Delete the folder called "build"
-Step 4: Go back 1 folder
-Step 5: Open the generated exe file
+Step 3: Go back 1 folder
+Step 4: Open the generated exe file
